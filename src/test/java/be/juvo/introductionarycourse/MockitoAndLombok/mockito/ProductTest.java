f6b43ca4e1e83363bbe3bf3cc70f996759dce7da
Paul_Gerarts <paul.gerarts@juvo.be>
2019-11-13 19:35:15 +0100

    - contains the answers to the excercises

--- src/test/java/be/juvo/introductionarycourse/MockitoAndLombok/mockito/ProductTest.java
+++ src/test/java/be/juvo/introductionarycourse/MockitoAndLombok/mockito/ProductTest.java
@@ -1,22 +1,39 @@
 package be.juvo.introductionarycourse.MockitoAndLombok.mockito;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class ProductTest {
 
-    //TODO: mock the mockito.Product class
+    private Product product;
 
-    @Test
-    public void buyThreeItemsTest(){
-        //buy three items that cost €1.5
-        //assert that the mock gives us the correct total price
+    @BeforeEach
+    public void setUp(){
+        product = mock(Product.class);
     }
 
     @Test
-    public void addFourProductsToCartTest(){
-        //assert that the addToCart method is called 4 times
+    public void buyThreeItemsTest() {
+        //given
+        int amount = 3;
+        Double totalValueOfPurchase = 4.5;
+        //when
+        when(product.buySomething(amount)).thenReturn(totalValueOfPurchase);
+        //then
+        assertThat(product.buySomething(amount)).isEqualTo(totalValueOfPurchase);
+    }
+
+    @Test
+    public void addFourProductsToCartTest() {
+        //given
+        for (int i = 0; i < 4; i++) {
+            product.addToCart(new Product());
+        }
+        //when-then
+        verify(product, times(4)).addToCart(any());
     }
 
 }
